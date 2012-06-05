f2657e82f082baa55e5207210723b2e3210ac52b
Sheena Gygax <sheenagyax@gmail.com>
2012-06-05 18:06:58 -0500

    Added Image Column to Photos - photo upload should work now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120605214348) do
+ActiveRecord::Schema.define(:version => 20120605230518) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20120605214348) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "questions", :force => true do |t|
