6c03b4243974f15ee96580dfb0a371274db892e8
Sheena Gygax <sheenagyax@gmail.com>
2012-06-09 16:18:56 -0500

    Remove the add image to photos migration

--- db/migrate/20120602212925_create_photos.rb
+++ db/migrate/20120602212925_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
       t.string :text
       t.integer :question_id
+      t.string :image
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,9 @@ ActiveRecord::Schema.define(:version => 20120609162157) do
   create_table "photos", :force => true do |t|
     t.string   "text"
     t.integer  "question_id"
-    t.string   "embed_url"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
-    t.string   "image"
   end
 
   create_table "questions", :force => true do |t|
