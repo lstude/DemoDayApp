fe03dc6ff27c8d67605eb7b66bc84a2f786fa501
Sheena Gygax <sheenagyax@gmail.com>
2012-06-09 11:31:15 -0500

    Removed embed_url column from Photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :embed_url, :question_id, :text, :image
+  attr_accessible :question_id, :text, :image
   
   belongs_to :user
   belongs_to :question
