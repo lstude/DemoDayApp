fed5f7da7aa6cf49a7f31f7a134c781b082f6439
Sheena Gygax <sheenagyax@gmail.com>
2012-06-09 12:59:36 -0500

    Add Rmagick to show thumbnail on photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -48,7 +48,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to profile_index_url, notice: 'Photo was successfully created.' }
         format.json { render json: @photo, status: :created, location: @photo }
       else
         format.html { render action: "new" }
