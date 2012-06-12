ac81869ce6b7110e33879658348982316751e8dc
Sheena Gygax <sheenagyax@gmail.com>
2012-06-12 16:07:38 -0500

    Added some jquery effects to the profile page

---
+++ app/assets/javascripts/jquery.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+
+$(".readMore").hide();
+
+// Slide Up & Down used before I grabbed the Accordion Feature
+		$(".title").click(function() {
+				if ($(this).next().is(":hidden")) {
+					$(this).next().slideDown("slow");
+				} else {
+								$(this).next().slideUp("slow");
+							} 
+	  });
+	
+});
+
